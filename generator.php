570b24227fe130bee95347a7b608e7534277067d
Skarbo <kris.skarbo@gmail.com>
2013-06-07 14:34:52 +0200

    Fix relative path bug

--- generator.php
+++ generator.php
@@ -6,7 +6,7 @@ $generator = new Generator( "localhost", "root", "", "aapent_test", Generator::M
 // // $generator->skipTables = array ( "building", "building_element", "building_element_type", "building_element_type_group",
 // //         "building_floor", "building_section", "building_debug", "debug", "error", "facility", "queue",
 // //         "schedule_entry_faculty", "schedule_entry_group" );
-$generator->onlyTables = array( "feature" );
-$generator->doGenerate( realpath( "./" ) );
+// $generator->onlyTables = array( "feature" );
+// $generator->doGenerate( realpath( "./" ) );
 
 ?>
