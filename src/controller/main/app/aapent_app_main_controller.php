2dccf13cd52d2588ecb1d5b998ee11dfd0f841e9
Skarbo <kris.skarbo@gmail.com>
2013-06-10 06:27:51 +0200

    Added Google Analytics

--- src/controller/main/app/aapent_app_main_controller.php
+++ src/controller/main/app/aapent_app_main_controller.php
@@ -62,9 +62,17 @@ var eventHandler, view, controller;
 $(document).ready(function() {
 	%s
 });
+
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+ga('create', '%s', '%s');
+ga('send', 'pageview');
 EOD;
 
-        $this->addJavascriptCode( sprintf( $code, $codeBody ) );
+        $this->addJavascriptCode( sprintf( $code, $codeBody, "UA-16538668-5", "krisskarbo.com" ) );
         $this->addJavascriptMap();
     }
 
